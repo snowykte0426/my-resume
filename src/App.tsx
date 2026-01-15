e55fcbbc91807428594fcf13876d637de7646a6f
Kim Tae Eun <snowykte0426@naver.com>
2026-01-15 20:25:47 +0900

    add ThemeToggle component to App for theme switching

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import './App.css'
 import './markdown.css'
 import Resume from './content/resume.mdx'
+import ThemeToggle from './components/ThemeToggle'
 
 function App() {
   return (
-    <div className="mdx-content">
-      <Resume />
-    </div>
+    <>
+      <ThemeToggle />
+      <div className="mdx-content">
+        <Resume />
+      </div>
+    </>
   )
 }
 
