1dff7338f31fb4493576093b027449e1bf5769b1
snowykte0426 <snowykte0426@naver.com>
2026-08-15 00:37:53 +0900

    add open source route; implement OpenSourceList component and routing logic

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,22 @@ import './markdown.css'
 import Resume from './content/resume.mdx'
 import ThemeToggle from './components/ThemeToggle'
 import PrintButton from './components/PrintButton'
+import OpenSourceList from './components/OpenSourceList'
 import { FootnoteProvider, FootnoteList } from './components/Footnote'
+import { useHashRoute } from './hooks/useHashRoute'
 
 function App() {
+  const route = useHashRoute()
+
+  if (route === '/open-source') {
+    return (
+      <>
+        <ThemeToggle />
+        <OpenSourceList />
+      </>
+    )
+  }
+
   return (
     <FootnoteProvider>
       <PrintButton />
